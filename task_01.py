38b8fe31ba4c82c2c9c9d46377ccbdfed29dbcb1
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-10-05 01:07:58 +0000

    not sure

---
+++ task_01.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""counting guests and tables"""
+
+
+def get_party_stats(families, table_size=6):
+    """returns total number of guests and total number of tables in tuple
+    arguments:
+        families: first argument.
+
+        table_size: second argument, has a default value 6
+
+        for loop: counting every guest name in every families and being added
+        to total_guests one by one in order.
+
+    returns:
+        returns sum of dataset(total_guests), and total number of tables needed.
+
+    examples:
+        >>>get_party_stats([['Jan'], ['Jen', 'Jess'], ['Jem', 'Jack', 'Janis']])
+        (6, 3)
+
+        >>>get_party_stats([['Jan'], ['Jen', 'Jess'], ['Jem', 'Jack', 'Janis']],
+        2)
+        (6, 4)
+    """
+
+    total_guests = 0
+    for guests in families:
+        total_guests = total_guests + len(guests)
+    total_tables = -((-len(families[0]) // table_size)
+                     + (-len(families[1]) // table_size)
+                     + (-len(families[2]) // table_size))
+    return total_guests, total_tables
