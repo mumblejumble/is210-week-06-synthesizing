e7f8f61f646b3277b73d73748d66f909be579759
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-10-05 03:08:19 +0000

    not final

--- task_02.py
+++ task_02.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""sending automated e-mails"""
+"""creating emails with for loop"""
 
 
-#appointment = [('Wiley', 'Monday, March 16, 2015'), ('Emily', 'Tuesday')]
-email = 'Dear {},\nI look forward to meeting with you on {}.\nBest,\nMe'
+def prepare_email(appointments):
+    """creates emails with for loop and DRY principle
 
-def prepare_email(name, time):
-    """generates email"""
-    print email.format(name, time)
+    arguments:
+        appointments: a list of two item tuples with the client's name and
+        their appointment times as members.
+
+        for loop: loops email statements and inserting list dataset everytime.
+
+    examples:
+        >>>prepare_email([('Jen', '2015'), ('Max', 'March 3')])
+        ['Dear Jen, \nI look forward to metting you with you on 2015.\nMe',
+        'Dear Max, \nI look forward to meeting you on March 3.\nBest\nMe']
+    """
+    statement = 'Dear {},\nI look forward to meeting with you on {}.\nBest,\nMe'
+    for appoint in appointments:
+        print statement.format(appoint[0], appoint[1])
