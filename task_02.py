cdb40db67a4834705ac7868e47ace99af3137210
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-10-05 05:15:19 +0000

    used append

--- task_02.py
+++ task_02.py
@@ -7,25 +7,29 @@ def prepare_email(appointments):
     """creates emails with for loop and DRY principle
 
     arguments:
-        appointments: a list of two item tuples with the client's name and
-        their appointment times as members.
+        appointments: would be passed by a list of two item tuples with the
+        client's name and their appointment times as members.
 
-        for loop: loops email statements and inserting list dataset everytime.
+        newlist: makes the returned a list.
 
-        newlist: creates a list of looped f_emails
+        for loop: loops formatted emailbody, and appends itself to the list
+        untill the end of the list.
 
     returns:
-        returns a list of 
+        returns a list of formatted loops.
 
     examples:
         >>>prepare_email([('Jen', '2015'), ('Max', 'March 3')])
         ['Dear Jen, \nI look forward to metting you with you on 2015.\nMe',
         'Dear Max, \nI look forward to meeting you on March 3.\nBest\nMe']
+        >>> prepare_email([('Julie', 'Jan 1'), ('Maggie', 'Feb 3'),
+        ('App', 'April 1')])
+        ['Dear Julie,\nI look forward to meeting with you on Jan 1.\nBest,\nMe',
+        'Dear Maggie,\nI look forward to meeting with you on Feb 3.\nBest,\nMe',
+        'Dear App,\nI look forward to meeting with you on April 1.\nBest,\nMe']
     """
-    statement = 'Dear {},\nI look forward to meeting with you on {}.\nBest,\nMe'
-
+    emailbody = 'Dear {},\nI look forward to meeting with you on {}.\nBest,\nMe'
+    newlist = []
     for appoint in appointments:
-        f_email = statement.format(appoint[0], appoint[1])
-        print f_email
-        newlist = [f_email, f_email]
+        newlist.append(emailbody.format(appoint[0], appoint[1]))
     return newlist
